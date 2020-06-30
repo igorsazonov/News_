9fa6fa46fb8cee79a12abb365cf08dcf0da3ee0b
igorsazonov <sazonovkerch@gmail.com>
2020-06-30 09:03:48 +0300

    Сделана частичная структура проекта, категории новостей, лого, лаунч скрин

---
+++ FreshNews/Controller/MainScreens/NewsFeedViewController.swift
@@ -0,0 +1,137 @@
+//
+//  NewsFeedViewController.swift
+//  FreshNews
+//
+//  Created by Игорь Сазонов on 05.06.2020.
+//  Copyright © 2020 Игорь Сазонов. All rights reserved.
+//
+
+import UIKit
+
+class NewsFeedViewController: UIViewController {
+
+    // MARK: - Variables
+    
+    @IBOutlet weak var containerView: UIView!
+    
+    @IBOutlet var buttons: [UIButton]!
+    
+    private lazy var  topViewController: TopViewController = {
+        var viewController = UIStoryboard(name: "Top", bundle: Bundle.main).instantiateViewController(withIdentifier: "topVc") as! TopViewController
+        return viewController
+    }()
+    
+    private lazy var  entertainViewController: EntertainViewController = {
+        var viewController = UIStoryboard(name: "Entertain", bundle: Bundle.main).instantiateViewController(withIdentifier: "entertainVc") as! EntertainViewController
+        return viewController
+    }()
+    
+    private lazy var  sportsViewController: SportsViewController = {
+        var viewController = UIStoryboard(name: "Sports", bundle: Bundle.main).instantiateViewController(withIdentifier: "sportsVc") as! SportsViewController
+        return viewController
+    }()
+    
+    private lazy var  generalViewController: GeneralViewController = {
+        var viewController = UIStoryboard(name: "General", bundle: Bundle.main).instantiateViewController(withIdentifier: "generalVc") as! GeneralViewController
+        return viewController
+    }()
+    
+    private lazy var  businessViewController: BusinessViewController = {
+        var viewController = UIStoryboard(name: "Business", bundle: Bundle.main).instantiateViewController(withIdentifier: "businessVc") as! BusinessViewController
+        return viewController
+    }()
+    
+    private lazy var  techViewController: TechViewController = {
+        var viewController = UIStoryboard(name: "Tech", bundle: Bundle.main).instantiateViewController(withIdentifier: "techVc") as! TechViewController
+        return viewController
+    }()
+    
+    private lazy var  scienceViewController: ScienceViewController = {
+        var viewController = UIStoryboard(name: "Science", bundle: Bundle.main).instantiateViewController(withIdentifier: "scienceVc") as! ScienceViewController
+        return viewController
+    }()
+    
+    private lazy var  healthViewController: HealthViewController = {
+        var viewController = UIStoryboard(name: "Health", bundle: Bundle.main).instantiateViewController(withIdentifier: "healthVc") as! HealthViewController
+        return viewController
+    }()
+    
+    
+    // MARK: - Function
+    
+    @objc func clicked(_ selectedButton: UIButton) {
+        var viewController: UIViewController? = nil
+        var index: Int? = nil
+        
+        index = selectedButton.tag
+        viewController = getController(forIndex: index!)!
+        
+        for button in buttons {
+            button.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+        }
+        
+        selectedButton.layer.backgroundColor = #colorLiteral(red: 0.9529411765, green: 0.6862745098, blue: 0.1333333333, alpha: 1)
+        add(asChildViewController: viewController!)
+    }
+   
+    private func add(asChildViewController viewController: UIViewController?) {
+        if viewController != nil {
+            addChild(viewController!)
+            containerView.addSubview(viewController!.view)
+            viewController!.view.frame = containerView.bounds
+            viewController!.view.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+            viewController!.didMove(toParent: self)
+        } else {
+            print("Error: viewController nil")
+        }
+    }
+    
+    private func remove(asChildViewController viewController: UIViewController?) {
+        if viewController != nil {
+            viewController!.willMove(toParent: nil)
+            viewController!.view.removeFromSuperview()
+            viewController!.removeFromParent()
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        if buttons.count > 0 {
+            let firstButton: UIButton = buttons[0]
+            add(asChildViewController: topViewController)
+                
+            for button in buttons {
+                button.layer.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+                button.layer.cornerRadius = 17
+                button.addTarget(self, action: #selector(clicked(_:)), for: UIControl.Event.touchUpInside)
+            }
+            firstButton.layer.backgroundColor = #colorLiteral(red: 0.9529411765, green: 0.6862745098, blue: 0.1333333333, alpha: 1)
+        }
+    }
+    
+    private func getController(forIndex index: Int) -> UIViewController? {
+        var viewController: UIViewController?
+        
+        switch index {
+        case 0:
+            viewController = topViewController
+        case 1:
+            viewController = entertainViewController
+        case 2:
+            viewController = sportsViewController
+        case 3:
+            viewController = generalViewController
+        case 4:
+            viewController = businessViewController
+        case 5:
+            viewController = techViewController
+        case 6:
+            viewController = scienceViewController
+        case 7:
+            viewController = healthViewController
+        default:
+            viewController = nil
+        }
+        return viewController
+    }
+}
