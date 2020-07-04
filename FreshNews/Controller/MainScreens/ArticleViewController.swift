89539db7db7757007ecfe10690f705bb4c064c58
igorsazonov <sazonovkerch@gmail.com>
2020-07-04 23:57:23 +0300

    changes for 04.07

--- FreshNews/Controller/MainScreens/ArticleViewController.swift
+++ FreshNews/Controller/MainScreens/ArticleViewController.swift
@@ -11,8 +11,7 @@ import Kingfisher
 
 class ArticleViewController: UIViewController {
     var article: Article?
-    let urlNotImage = "https://cdn.freelance.ru/img/portfolio/pics/00/39/04/3736676.jpg?mt=cb1c2bed" //?
-   // var description: String?
+    let urlNotImage = "https://cdn.freelance.ru/img/portfolio/pics/00/39/04/3736676.jpg?mt=cb1c2bed"
     @IBOutlet weak var articleImage: UIImageView!
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var descriptionLabel: UILabel!
